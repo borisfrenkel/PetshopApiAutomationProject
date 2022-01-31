ddf9bbe6b56fba3ff3456e5bc5c0ac387fcee599
borisfrenkel <bfrenkel@gmail.com>
2022-01-31 16:07:26 +0100

    moving AfterTestExecutionCallback to BaseTest

--- src/test/java/petshop/tests/BaseTest.java
+++ src/test/java/petshop/tests/BaseTest.java
@@ -1,19 +1,21 @@
 package petshop.tests;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import petshop.helpers.HTTPClientHelper;
 import petshop.helpers.JSONHelper;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class BaseTest {
+public class BaseTest implements AfterTestExecutionCallback {
     protected HTTPClientHelper httpClientHelper;
     protected JSONHelper jsonHelper;
     // assumes the current class is called MyLogger
-    protected Logger LOGGER = null;
+    protected static Logger LOGGER = null;
 
     @BeforeAll
     public void beforeAll(){
@@ -27,9 +29,13 @@ public class BaseTest {
         LOGGER.log(Level.INFO, "Test " + testInfo.getDisplayName() + " is started");
     }
 
-    @AfterEach
-    public void afterEach(TestInfo testInfo){
-        LOGGER.log(Level.INFO, "Test " + testInfo.getDisplayName() + " is started");
+    @Override
+    public void afterTestExecution(ExtensionContext extensionContext) throws Exception {
+        if(extensionContext.getExecutionException().isPresent()){
+            LOGGER.log(Level.INFO, "Test " + extensionContext.getDisplayName() + " failed with " + extensionContext.getExecutionException());
+        }
+        else {
+            LOGGER.log(Level.INFO, "Test " + extensionContext.getDisplayName() + " succeeded");
+        }
     }
-
 }
--- src/test/java/petshop/tests/FindByStatusTests.java
+++ src/test/java/petshop/tests/FindByStatusTests.java
@@ -12,13 +12,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@ExtendWith(PetshopAfterTestExecutionCallback.class)
+@ExtendWith(BaseTest.class)
 public class FindByStatusTests extends BaseTest {
     @Test
     public void callFindByStatusApiWithStatusEqAvailableVerifyHttpStatus() throws IOException, ParseException {
         CloseableHttpResponse response = httpClientHelper.Get("https://petstore3.swagger.io/api/v3/pet/findByStatus", "status=available");
 
-        Assertions.assertEquals(HttpStatus.SC_BAD_REQUEST, response.getCode(), "Test failed");
+        Assertions.assertEquals(HttpStatus.SC_OK, response.getCode(), "Test failed");
     }
 
     @Test
